dc2a314cded1cadc2d62ec8d421d19a25028cf95
James Wang <jw@slickage.com>
2016-11-28 01:55:19 -1000

    Posts with no tsv

--- priv/repo/migrations/20161128110637_create_posts.exs
+++ priv/repo/migrations/20161128110637_create_posts.exs
@@ -2,6 +2,20 @@ defmodule Epoch.Repo.Migrations.CreatePosts do
   use Ecto.Migration
 
   def change do
+    create table(:posts, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :thread_id, references(:threads, type: :uuid)
+      add :user_id, references(:users, type: :uuid)
+      add :title, :string
+      add :raw_body, :text
+      add :body, :text
+      add :deleted, :boolean
+      add :locked, :boolean
+      add :position, :integer
+      add :tsv, :tsvector
+      add :imported_at, :timestamp
+      timestamps
+    end
 
   end
 end
