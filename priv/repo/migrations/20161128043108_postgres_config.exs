b2ccf9a0673e981e3d512f4e00e219e0143b6f3f
James Wang <jw@slickage.com>
2017-06-23 15:56:25 -1000

    Schemas created in postgres config

--- priv/repo/migrations/20161128043108_postgres_config.exs
+++ priv/repo/migrations/20161128043108_postgres_config.exs
@@ -2,7 +2,14 @@ defmodule Epoch.Repo.Migrations.PostgresConfig do
   use Ecto.Migration
 
   def change do
-    execute "CREATE EXTENSION IF NOT EXISTS citext"
-    execute "CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\""
+    execute "CREATE SCHEMA administration"
+    execute "CREATE SCHEMA ads"
+    execute "CREATE SCHEMA factoids"
+    execute "CREATE SCHEMA mentions"
+    execute "CREATE SCHEMA mod"
+    execute "CREATE SCHEMA users"
+    execute "CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public"
+    execute "CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\" WITH SCHEMA public"
+    execute "CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog"
   end
 end
