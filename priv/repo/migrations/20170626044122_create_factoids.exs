4abfe27ce5de28a3f0310aaa7e2647eaabafaf8e
James Wang <jw@slickage.com>
2017-06-25 18:51:47 -1000

    Removed sql in comment

--- priv/repo/migrations/20170626044122_create_factoids.exs
+++ priv/repo/migrations/20170626044122_create_factoids.exs
@@ -1,14 +1,6 @@
 defmodule Epoch.Repo.Migrations.CreateFactoids do
   use Ecto.Migration
 
-  # CREATE TABLE factoids (
-  #     id uuid DEFAULT uuid_generate_v4() NOT NULL,
-  #     text text NOT NULL,
-  #     enabled boolean DEFAULT true,
-  #     created_at timestamp with time zone,
-  #     updated_at timestamp with time zone
-  # );
-  #
   def change do
     create table(:factoids, primary_key: false) do
       add :id, :binary_id, primary_key: true, default: fragment("uuid_generate_v4()")
