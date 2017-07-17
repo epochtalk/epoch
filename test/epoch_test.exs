431bcf3380d357bde3cf5af2bad70fb17558f94d
James Wang <jw@slickage.com>
2017-07-17 10:02:52 -0700

    add board/thread/post test

--- test/epoch_test.exs
+++ test/epoch_test.exs
@@ -2,7 +2,49 @@ defmodule EpochTest do
   use ExUnit.Case
   doctest Epoch
 
-  test "Epoch Test" do
-    assert 1 == 1
+  test "board" do
+    b = %Epoch.Board{
+      name: "Testing Board",
+      description: "Testing grounds for discussion",
+      created_at: Ecto.DateTime.utc(),
+      updated_at: Ecto.DateTime.utc()
+    } |> Epoch.Repo.insert!
+    assert b.name == "Testing Board"
+  end
+
+  test "thread" do
+    b = %Epoch.Board{
+      name: "Testing Board",
+      description: "Testing grounds for discussion",
+      created_at: Ecto.DateTime.utc(),
+      updated_at: Ecto.DateTime.utc()
+    } |> Epoch.Repo.insert!
+    t = %Epoch.Thread{
+      board: b,
+      created_at: Ecto.DateTime.utc(),
+      updated_at: Ecto.DateTime.utc()
+    } |> Epoch.Repo.insert!
+    assert b.id == t.board_id
+  end
+
+  test "post" do
+    b = %Epoch.Board{
+      name: "Testing Board",
+      description: "Testing grounds for discussion",
+      created_at: Ecto.DateTime.utc(),
+      updated_at: Ecto.DateTime.utc()
+    } |> Epoch.Repo.insert!
+    t = %Epoch.Thread{
+      board: b,
+      created_at: Ecto.DateTime.utc(),
+      updated_at: Ecto.DateTime.utc()
+    } |> Epoch.Repo.insert!
+    assert b.id == t.board_id
+    p = %Epoch.Post{
+      thread: t,
+      created_at: Ecto.DateTime.utc(),
+      updated_at: Ecto.DateTime.utc()
+    } |> Epoch.Repo.insert!
+    assert t.id == p.thread_id
   end
 end
