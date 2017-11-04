ed8f7cef93ffb497311b31f1778ed10f035cb1c1
James Wang <jw@slickage.com>
2017-11-03 15:41:12 -1000

    dev config secrets

--- config/dev.exs
+++ config/dev.exs
@@ -36,3 +36,7 @@ config :logger, :console, format: "[$level] $message\n"
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
+
+if File.exists?("config/dev.secret.exs") do
+  import_config "dev.secret.exs"
+end
