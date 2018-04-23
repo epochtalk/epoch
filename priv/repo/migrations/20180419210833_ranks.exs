06c6bcf2ceaaf4c73347335dbdbc00b612ea10fb
Anthony Kinsey <anthony@slickage.com>
2018-04-23 12:10:48 -1000

    Modify rank schema
    * Rank schema has been modified to support multiple metric
      thresholds.
    
    * Schema has also been modified to not store user ranks in
      the database. Instead we are going to calculate user rank
      on the fly.

--- priv/repo/migrations/20180419210833_ranks.exs
+++ priv/repo/migrations/20180419210833_ranks.exs
@@ -4,18 +4,15 @@ defmodule Epoch.Repo.Migrations.Ranks do
   def change do
     create table(:ranks, primary_key: false) do
       add(:id, :binary_id, primary_key: true, default: fragment("uuid_generate_v4()"))
-      add(:rank_name, :text, null: false)
-      add(:threshold, :integer, null: false)
+      add(:name, :text, null: false)
+      add(:ranking, :integer, null: false)
     end
 
-    create(index(:ranks, [:threshold]))
+    create(unique_index(:ranks, [:ranking]))
 
-    create table(:users_ranks, primary_key: false) do
-      add(:user_id, references(:users, type: :uuid, on_delete: :delete_all))
-      add(:rank_id, references(:ranks, type: :uuid))
+    create table(:ranks_thresholds, primary_key: false) do
+      add(:rank_id, references(:ranks, type: :uuid, on_delete: :delete_all))
+      add(:thresholds, :jsonb)
     end
-
-    create(index(:users_ranks, [:user_id]))
-    create(index(:users_ranks, [:rank_id]))
   end
 end
