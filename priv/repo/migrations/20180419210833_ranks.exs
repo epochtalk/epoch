a504d8b769d263ffbc7594f047129f173c0e61d7
Anthony Kinsey <anthony@slickage.com>
2018-04-23 13:45:01 -1000

    Further simplify rank schema design
    * Updated schema to bare minimum

--- priv/repo/migrations/20180419210833_ranks.exs
+++ priv/repo/migrations/20180419210833_ranks.exs
@@ -3,16 +3,14 @@ defmodule Epoch.Repo.Migrations.Ranks do
 
   def change do
     create table(:ranks, primary_key: false) do
-      add(:id, :binary_id, primary_key: true, default: fragment("uuid_generate_v4()"))
       add(:name, :text, null: false)
-      add(:ranking, :integer, null: false)
+      add(:number, :integer, null: false)
     end
 
-    create(unique_index(:ranks, [:ranking]))
+    create(unique_index(:ranks, [:number]))
 
-    create table(:ranks_thresholds, primary_key: false) do
-      add(:rank_id, references(:ranks, type: :uuid, on_delete: :delete_all))
-      add(:thresholds, :jsonb)
+    create table(:metric_rank_map, primary_key: false) do
+      add(:map, :jsonb, null: false)
     end
   end
 end
