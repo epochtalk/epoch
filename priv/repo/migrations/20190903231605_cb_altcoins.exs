f9690b24b2f4ad7ee79915fcb8713239db55469c
Anthony Kinsey <anthony@slickage.com>
2019-09-03 15:30:37 -1000

    Updated coinbistro migration

--- priv/repo/migrations/20190903231605_cb_altcoins.exs
+++ priv/repo/migrations/20190903231605_cb_altcoins.exs
@@ -5,41 +5,36 @@ defmodule Elixir.Epoch.Repo.Migrations.CbAltcoins do
   def up do
     execute "CREATE SCHEMA " <> @schema_prefix
 
-    create table(:cb_cryptos, [prefix: @schema_prefix]) do
+    create table(:cb_projects, [prefix: @schema_prefix]) do
       add(:name, :text, null: false)
+      add(:type, :text, null: false)
       add(:description, :text, null: false)
-      add(:tracker, :text, null: false)
+      add(:bct_ann, :text, null: false)
+      add(:ann_date, :text, null: false)
+      add(:website, :text, null: false)
       add(:flags, {:array, :uuid}, default: [])
-    end
-
-    create table(:cb_exchanges, [prefix: @schema_prefix]) do
-      add(:name, :text, null: false)
-      add(:description, :text, null: false)
-      add(:tracker, :text, null: false)
-      add(:flags, {:array, :uuid}, default: [])
-    end
-
-    create table(:cb_services, [prefix: @schema_prefix]) do
-      add(:name, :text, null: false)
-      add(:description, :text, null: false)
-      add(:tracker, :text, null: false)
-      add(:flags, {:array, :uuid}, default: [])
+      add(:badges, {:array, :uuid}, default: [])
     end
 
     create table(:cb_flags, [prefix: @schema_prefix]) do
       add(:name, :text, null: false)
       add(:description, :text, null: false)
+      add(:user_text, :text, null: false)
+      add(:user_url, :text, null: false)
       add(:supporting, {:array, :uuid}, default: [])
       add(:opposing, {:array, :uuid}, default: [])
     end
 
+    create table(:cb_badges, [prefix: @schema_prefix]) do
+      add(:name, :text, null: false)
+      add(:description, :text, null: false)
+    end
   end
 
   def down do
-    drop table(:cb_cryptos, [prefix: @schema_prefix])
-    drop table(:cb_exchanges, [prefix: @schema_prefix])
-    drop table(:cb_services, [prefix: @schema_prefix])
+    drop table(:cb_projects, [prefix: @schema_prefix])
     drop table(:cb_flags, [prefix: @schema_prefix])
+    drop table(:cb_badges, [prefix: @schema_prefix])
     execute "DROP SCHEMA " <> @schema_prefix
   end
 end
