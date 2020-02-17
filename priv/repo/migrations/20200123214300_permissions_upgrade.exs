2fa1c4acb74249e8e092e541cdfa9fc10fd994b4
Anthony Kinsey <anthonyjk@gmail.com>
2020-02-17 12:15:31 -1000

    feat(permissions-upgrade): update roles migration to support new role diff

--- priv/repo/migrations/20200123214300_permissions_upgrade.exs
+++ priv/repo/migrations/20200123214300_permissions_upgrade.exs
@@ -2,8 +2,10 @@ defmodule Epoch.Repo.Migrations.PermissionsUpgrade do
   use Ecto.Migration
 
   def change do
+    rename table(:roles), :permissions, to: :custom_permissions
+
     alter table(:roles) do
-      add :custom_permissions, :jsonb
+      add :base_permissions, :jsonb
     end
   end
 end
