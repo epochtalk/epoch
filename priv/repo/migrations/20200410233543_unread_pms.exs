464022caddeb753874e6fb096892bf127b043e2d
Anthony Kinsey <anthonyjk@gmail.com>
2020-04-14 11:31:03 -1000

    fix: add default empty array to read_by_user_ids pm columns

--- priv/repo/migrations/20200410233543_unread_pms.exs
+++ priv/repo/migrations/20200410233543_unread_pms.exs
@@ -3,10 +3,10 @@ defmodule Epoch.Repo.Migrations.UnreadPms do
   @schema_prefix "messages"
   def change do
     alter table(:private_conversations, [prefix: @schema_prefix]) do
-      add :read_by_user_ids, {:array, :uuid}
+      add :read_by_user_ids, {:array, :uuid}, default: []
     end
     alter table(:private_messages, [prefix: @schema_prefix]) do
-      add :read_by_user_ids, {:array, :uuid}
+      add :read_by_user_ids, {:array, :uuid}, default: []
     end
   end
 end
