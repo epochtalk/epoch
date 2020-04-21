a5f1c4c7ccd3189a33435f8b113d4083ccf08326
Anthony Kinsey <anthonyjk@gmail.com>
2020-04-21 13:23:21 -1000

    feat: add new table messages.ignored to store users ignored pm settings

---
+++ priv/repo/migrations/20200421225634_pm_ignored.exs
@@ -0,0 +1,14 @@
+defmodule Epoch.Repo.Migrations.PmIgnored do
+  use Ecto.Migration
+  @schema_prefix "messages"
+  def change do
+    create table(:ignored, [prefix: @schema_prefix, primary_key: false]) do
+      add :user_id, references(:users, type: :uuid, on_delete: :delete_all, prefix: "public"), null: false
+      add :ignored_user_id, references(:users, type: :uuid, on_delete: :delete_all, prefix: "public"), null: false
+    end
+
+    create index(:ignored, [:user_id], prefix: @schema_prefix)
+    create index(:ignored, [:ignored_user_id], prefix: @schema_prefix)
+    create unique_index(:ignored, [:user_id, :ignored_user_id], prefix: @schema_prefix)
+  end
+end
