92aab3b494e2a7e8c6e8cbc2e92ebd53e2086906
Anthony Kinsey <anthonyjk@gmail.com>
2020-08-25 14:26:41 -1000

    fix: tests to work with board slugs

--- test/epoch_test.exs
+++ test/epoch_test.exs
@@ -5,6 +5,7 @@ defmodule EpochTest do
   def create_board(name \\ "Testing Board") do
     %Epoch.Board{
       name: name,
+      slug: :crypto.strong_rand_bytes(5) |> Base.url_encode64 |> binary_part(0, 5),
       description: "Testing grounds for discussion",
       created_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second),
       updated_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)
--- test/epoch_test/post_test.exs
+++ test/epoch_test/post_test.exs
@@ -5,6 +5,7 @@ defmodule EpochTest.PostTest do
   test "post" do
     b = %Epoch.Board{
       name: "Testing Board",
+      slug: :crypto.strong_rand_bytes(5) |> Base.url_encode64 |> binary_part(0, 5),
       description: "Testing grounds for discussion",
       created_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second),
       updated_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)
