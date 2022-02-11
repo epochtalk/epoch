d8a6e498d293433690d26ad22e58acb867014bf7
Bronson Oka <boka@slickage.com>
2022-02-10 15:22:32 -1000

    feat(auth): stub username_taken
    still need to add to json

--- lib/epoch_web/controllers/auth_controller.ex
+++ lib/epoch_web/controllers/auth_controller.ex
@@ -1,7 +1,13 @@
 defmodule EpochWeb.Controllers.AuthController do
   use EpochWeb, :controller
+  alias Epoch.User
 
   def username(conn, %{"username" => username}) do
+    username_taken = username
+    |> User.with_username_exists?
+
+    IO.puts(username_taken)
+
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(200, """
