c5aa90daf7ac17d651422c38ad69f023336ad700
Bronson Oka <boka@slickage.com>
2022-02-10 17:52:01 -1000

    feat(auth): add email route to router

--- lib/epoch_web/router.ex
+++ lib/epoch_web/router.ex
@@ -10,6 +10,7 @@ defmodule EpochWeb.Router do
     get "/motd", RootController, :motd
     get "/boards/:slug/id", BoardController, :id
     get "/register/username/:username", AuthController, :username
+    get "/register/email/:email", AuthController, :email
     resources "/boards", BoardController, only: [:index, :show]
     resources "/threads", ThreadController, only: [:index, :show]
     resources "/posts", PostController, only: [:index, :show]
