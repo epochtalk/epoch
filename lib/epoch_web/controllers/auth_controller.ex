9408efba0e95685423f075fa03ff47eaa82dddc6
Bronson Oka <boka@slickage.com>
2022-02-10 17:50:45 -1000

    feat(auth): implement respond_ok
    encode data into json and respond 200

--- lib/epoch_web/controllers/auth_controller.ex
+++ lib/epoch_web/controllers/auth_controller.ex
@@ -2,16 +2,21 @@ defmodule EpochWeb.Controllers.AuthController do
   use EpochWeb, :controller
   alias Epoch.User
 
+  def respond_ok(data, conn) do
+    Jason.encode(data)
+    |> case do
+      {:ok, encoded_data} ->
+        conn
+        |> put_resp_content_type("application/json")
+        |> send_resp(200, encoded_data)
+      _ ->
+        IO.puts("uh oh...")
+    end
+  end
   def username(conn, %{"username" => username}) do
     username_taken = username
     |> User.with_username_exists?
 
-    IO.puts(username_taken)
-
-    conn
-    |> put_resp_content_type("application/json")
-    |> send_resp(200, """
-    {"found": false}
-    """)
+    respond_ok(%{ found: username_taken }, conn)
   end
 end
