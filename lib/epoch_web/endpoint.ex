b652428519b6288c7abcbcd9f68c55525b771bb7
Bronson Oka <boka@slickage.com>
2022-02-24 13:50:22 -1000

    feat(auth): add Plug.Session to endpoint
    enables :fetch_session in pipeline

--- lib/epoch_web/endpoint.ex
+++ lib/epoch_web/endpoint.ex
@@ -1,6 +1,16 @@
 defmodule EpochWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :epoch
 
+  # The session will be stored in the cookie and signed,
+  # this means its contents can be read but not tampered with.
+  # Set :encryption_salt if you would also like to encrypt it.
+  # Generate salt:  mix phx.gen.secret 32
+  @session_options [
+    store: :cookie,
+    key: "_epoch_key",
+    signing_salt: "xt5Mn4WjOYETgmQKYdXGG2dtUen3Yj55"
+  ]
+
   plug Corsica, origins: "*", allow_headers: :all
   plug Plug.RequestId
   plug Plug.Parsers,
@@ -10,5 +20,6 @@ defmodule EpochWeb.Endpoint do
 
   plug Plug.MethodOverride
   plug Plug.Head
+  plug Plug.Session, @session_options
   plug EpochWeb.Router
 end
