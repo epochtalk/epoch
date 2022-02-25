3e1e538d3051c78d31f9a77b083e1cdf9d527e7f
Bronson Oka <boka@slickage.com>
2022-02-24 15:45:26 -1000

    feat(auth): add browser pipeline
    revert/move security plugs from api pipeline to browser pipeline

--- lib/epoch_web/router.ex
+++ lib/epoch_web/router.ex
@@ -1,11 +1,18 @@
 defmodule EpochWeb.Router do
   use EpochWeb, :router
 
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    # plug :put_root_layout, {EpochWeb.LayoutView, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug :fetch_current_user
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
-    plug :put_secure_browser_headers
-    plug :fetch_session
-    plug :protect_from_forgery
   end
 
   scope "/api", EpochWeb do
