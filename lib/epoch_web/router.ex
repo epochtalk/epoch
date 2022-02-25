1f2c7c2e1bd0df19937e8386607af79496c8f06a
Bronson Oka <boka@slickage.com>
2022-02-24 15:44:54 -1000

    feat(auth): add authentication routes
    from gen_auth

--- lib/epoch_web/router.ex
+++ lib/epoch_web/router.ex
@@ -12,12 +12,44 @@ defmodule EpochWeb.Router do
     pipe_through :api
     get "/motd", RootController, :motd
     get "/boards/:slug/id", BoardController, :id
-    get "/register/username/:username", AuthController, :username
-    get "/register/email/:email", AuthController, :email
-    post "/register", AuthController, :register
+    # get "/register/username/:username", AuthController, :username
+    # get "/register/email/:email", AuthController, :email
+    # post "/register", AuthController, :register
     resources "/boards", BoardController, only: [:index, :show]
     resources "/threads", ThreadController, only: [:index, :show]
     resources "/posts", PostController, only: [:index, :show]
     resources "/users", UserController, only: [:show]
+  end
+
+  ## Authentication routes
+  scope "/", EpochWeb do
+    pipe_through [:api, :redirect_if_user_is_authenticated]
+
+    get "/users/register", UserRegistrationController, :new
+    post "/users/register", UserRegistrationController, :create
+    get "/users/log_in", UserSessionController, :new
+    post "/users/log_in", UserSessionController, :create
+    get "/users/reset_password", UserResetPasswordController, :new
+    post "/users/reset_password", UserResetPasswordController, :create
+    get "/users/reset_password/:token", UserResetPasswordController, :edit
+    put "/users/reset_password/:token", UserResetPasswordController, :update
+  end
+
+  scope "/", EpochWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    get "/users/settings", UserSettingsController, :edit
+    put "/users/settings", UserSettingsController, :update
+    get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
+  end
+
+  scope "/", EpochWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+    get "/users/confirm", UserConfirmationController, :new
+    post "/users/confirm", UserConfirmationController, :create
+    get "/users/confirm/:token", UserConfirmationController, :edit
+    post "/users/confirm/:token", UserConfirmationController, :update
   end
 end
