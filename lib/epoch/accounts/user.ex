9b51e155f3c88ee27e9b40b7d847d9cf3760515f
Bronson Oka <boka@slickage.com>
2022-02-24 17:13:15 -1000

    feat(auth): add validations for user changeset
    validate email, verify unique email
    validate password, and maybe hash password

--- lib/epoch/accounts/user.ex
+++ lib/epoch/accounts/user.ex
@@ -27,8 +27,8 @@ defmodule Epoch.Accounts.User do
     user
     |> cast(attrs, [:id, :email, :username, :created_at, :updated_at, :deleted, :malicious_score])
     |> unique_constraint(:id, name: :users_pkey)
-    |> unique_constraint(:email)
-    |> unique_constraint(:username)
+    |> validate_email()
+    |> validate_password(opts)
   end
 
   def with_username_exists?(username) do
@@ -45,5 +45,37 @@ defmodule Epoch.Accounts.User do
   end
   def insert(%User{} = user) do
     Repo.insert(user)
+  end
+
+  defp validate_email(changeset) do
+    changeset
+    |> validate_required([:email])
+    |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "must have the @ sign and no spaces")
+    |> validate_length(:email, max: 160)
+    |> unsafe_validate_unique(:email, Epochtalk.Repo)
+    |> unique_constraint(:email)
+  end
+
+  defp validate_password(changeset, opts) do
+    changeset
+    |> validate_required([:password])
+    |> validate_length(:password, min: 12, max: 72)
+    # |> validate_format(:password, ~r/[a-z]/, message: "at least one lower case character")
+    # |> validate_format(:password, ~r/[A-Z]/, message: "at least one upper case character")
+    # |> validate_format(:password, ~r/[!?@#$%^&*_0-9]/, message: "at least one digit or punctuation character")
+    |> maybe_hash_password(opts)
+  end
+
+  defp maybe_hash_password(changeset, opts) do
+    hash_password? = Keyword.get(opts, :hash_password, true)
+    password = get_change(changeset, :password)
+
+    if hash_password? && password && changeset.valid? do
+      changeset
+      |> put_change(:hashed_password, Argon2.hash_pwd_salt(password))
+      |> delete_change(:password)
+    else
+      changeset
+    end
   end
 end
