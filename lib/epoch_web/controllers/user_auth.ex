8607f9712817bad57dbe6bca0679e2b496f61163
Bronson Oka <boka@slickage.com>
2022-02-24 14:58:17 -1000

    feat(auth): implement user auth
    from gen_auth

--- lib/epoch_web/controllers/user_auth.ex
+++ lib/epoch_web/controllers/user_auth.ex
@@ -1,50 +1,116 @@
-defmodule EpochWeb.Controllers.AuthController do
-  use EpochWeb, :controller
-  alias Epoch.Accounts.User
+defmodule EpochWeb.Controllers.UserAuth do
+  import Plug.Conn
+  import Phoenix.Controller
 
-  def respond_ok(data, conn) do
-    Jason.encode(data)
-    |> case do
-      {:ok, encoded_data} ->
-        conn
-        |> put_resp_content_type("application/json")
-        |> send_resp(200, encoded_data)
-      _ ->
-        IO.puts("uh oh...")
+  alias Epoch.Accounts
+  alias Epoch.Router.Helpers, as: Routes
+
+  # Make the remember me cookie valid for 60 days.
+  # If you want bump or reduce this value, also change
+  # the token expiry itself in UserToken.
+  @max_age 60 * 60 * 24 * 60
+  @remember_me_cookie "_epoch_web_user_remember_me"
+  @remember_me_options [sign: true, max_age: @max_age, same_site: "Lax"]
+
+  @doc """
+  Logs the user in.
+
+  It renews the session ID and clears the whole session
+  to avoid fixation attacks. See the renew_session
+  function to customize this behaviour.
+
+  It also sets a `:live_socket_id` key in the session,
+  so LiveView sessions are identified and automatically
+  disconnected on log out. The line can be safely removed
+  if you are not using LiveView.
+  """
+  def log_in_user(conn, user, params \\ %{}) do
+    token = Accounts.generate_user_session_token(user)
+    user_return_to = get_session(conn, :user_return_to)
+
+    conn
+    |> renew_session()
+    |> put_session(:user_token, token)
+    |> put_session(:live_socket_id, "users_sessions:#{Base.url_encode64(token)}")
+    |> maybe_write_remember_me_cookie(token, params)
+    |> redirect(to: user_return_to || signed_in_path(conn))
+  end
+
+  defp maybe_write_remember_me_cookie(conn, token, %{"remember_me" => "true"}) do
+    put_resp_cookie(conn, @remember_me_cookie, token, @remember_me_options)
+  end
+
+  defp maybe_write_remember_me_cookie(conn, _token, _params) do
+    conn
+  end
+
+  # This function renews the session ID and erases the whole
+  # session to avoid fixation attacks. If there is any data
+  # in the session you may want to preserve after log in/log out,
+  # you must explicitly fetch the session data before clearing
+  # and then immediately set it after clearing, for example:
+  #
+  #     defp renew_session(conn) do
+  #       preferred_locale = get_session(conn, :preferred_locale)
+  #
+  #       conn
+  #       |> configure_session(renew: true)
+  #       |> clear_session()
+  #       |> put_session(:preferred_locale, preferred_locale)
+  #     end
+  #
+  defp renew_session(conn) do
+    conn
+    |> configure_session(renew: true)
+    |> clear_session()
+  end
+
+  @doc """
+  Logs the user out.
+
+  It clears all session data for safety. See renew_session.
+  """
+  def log_out_user(conn) do
+    user_token = get_session(conn, :user_token)
+    user_token && Accounts.delete_session_token(user_token)
+
+    if live_socket_id = get_session(conn, :live_socket_id) do
+      EpochWeb.Endpoint.broadcast(live_socket_id, "disconnect", %{})
+    end
+
+    conn
+    |> renew_session()
+    |> delete_resp_cookie(@remember_me_cookie)
+    |> redirect(to: "/")
+  end
+
+  @doc """
+  Authenticates the user by looking into the session
+  and remember me token.
+  """
+  def fetch_current_user(conn, _opts) do
+    {user_token, conn} = ensure_user_token(conn)
+    user = user_token && Accounts.get_user_by_session_token(user_token)
+    assign(conn, :current_user, user)
+  end
+
+  defp ensure_user_token(conn) do
+    if user_token = get_session(conn, :user_token) do
+      {user_token, conn}
+    else
+      conn = fetch_cookies(conn, signed: [@remember_me_cookie])
+
+      if user_token = conn.cookies[@remember_me_cookie] do
+        {user_token, put_session(conn, :user_token, user_token)}
+      else
+        {nil, conn}
+      end
     end
   end
-  def username(conn, %{"username" => username}) do
-    username_taken = username
-    |> User.with_username_exists?
 
-    respond_ok(%{ found: username_taken }, conn)
-  end
-  def email(conn, %{"email" => email}) do
-    email_taken = email
-    |> User.with_email_exists?
-
-    respond_ok(%{ found: email_taken }, conn)
-  end
-  def register(conn, %{"username" => username, "email" => email, "password" => password}) do
-    %User{username: username, email: email, passhash: password}
-    |> User.insert
-    |> case do
-      {:ok, user} ->
-        %{
-          token: user.confirmation_token,
-          id: user.id,
-          username: user.username,
-          # TODO(boka): fill in these fields
-          avatar: "", # user.avatar
-          permissions: %{}, # user.permissions
-          roles: %{} # user.roles
-        }
-        |> respond_ok(conn)
-      # TODO(boka): handle errors
-      {:error, error} ->
-        respond_ok(error, conn)
-    end
-  end
+  @doc """
+  Used for routes that require the user to not be authenticated.
+  """
   def redirect_if_user_is_authenticated(conn, _opts) do
     if conn.assigns[:current_user] do
       conn
@@ -54,6 +120,30 @@ defmodule EpochWeb.Controllers.AuthController do
       conn
     end
   end
+
+  @doc """
+  Used for routes that require the user to be authenticated.
+
+  If you want to enforce the user email is confirmed before
+  they use the application at all, here would be a good place.
+  """
+  def require_authenticated_user(conn, _opts) do
+    if conn.assigns[:current_user] do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must log in to access this page.")
+      |> maybe_store_return_to()
+      |> redirect(to: Routes.user_session_path(conn, :new))
+      |> halt()
+    end
+  end
+
+  defp maybe_store_return_to(%{method: "GET"} = conn) do
+    put_session(conn, :user_return_to, current_path(conn))
+  end
+
+  defp maybe_store_return_to(conn), do: conn
 
   defp signed_in_path(_conn), do: "/"
 end
