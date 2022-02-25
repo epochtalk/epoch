f8dae8d5d2be087acfebc90a01ee0d0022738ec1
Bronson Oka <boka@slickage.com>
2022-02-24 16:59:55 -1000

    refactor(auth): comment-remove fetch_live_flash
    not necessary, since we're not implementing the frontend

--- lib/epoch_web/router.ex
+++ lib/epoch_web/router.ex
@@ -6,7 +6,7 @@ defmodule EpochWeb.Router do
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
-    plug :fetch_live_flash
+    # plug :fetch_live_flash
     # plug :put_root_layout, {EpochWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
