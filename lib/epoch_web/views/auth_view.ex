5717f5d9d5dd6e9577cb41f10c5902b66d7e8acf
unenglishable <unenglishable@gmail.com>
2022-04-08 17:34:18 -1000

    feat(auth): add show.json for auth_view
    used to render user on return from register

--- lib/epoch_web/views/auth_view.ex
+++ lib/epoch_web/views/auth_view.ex
@@ -4,4 +4,17 @@ defmodule EpochWeb.AuthView do
   def render("search.json", %{found: found}) do
     %{found: found}
   end
+  def render("show.json", %{user: user}) do
+    user |> user_json()
+  end
+  def user_json(user) do
+    %{
+      id: user.id,
+      username: user.username,
+          # TODO(boka): fill in these fields
+      avatar: "", # user.avatar
+      permissions: %{}, # user.permissions
+      roles: %{} # user.roles
+    }
+  end
 end
