a1c7d5642cbf6293550c83b7ce658a2f821f762d
unenglishable <unenglishable@gmail.com>
2022-04-19 16:55:13 -1000

    feat(auth): log_in_user from genauth
    differs, returning json instead of web page
    kept commented behaviour for now

--- lib/epoch_web/controllers/auth_controller.ex
+++ lib/epoch_web/controllers/auth_controller.ex
@@ -31,4 +31,17 @@ defmodule EpochWeb.AuthController do
         |> render("400.json", %{message: inspect(changeset.errors)})
     end
   end
+  defp log_in_user(conn, user, params \\ %{}) do
+    token = user.username # Accounts.generate_user_session_token(user)
+    # user_return_to = get_session(conn, :user_return_to)
+    user = Map.put(user, :token, token)
+
+    conn
+    # |> renew_session()
+    # |> put_session(:user_token, token)
+    # |> put_session(:live_socket_id, "users_sessions:#{Base.url_encode64(token)}")
+    # |> maybe_write_remember_me_cookie(token, params)
+    # |> redirect(to: user_return_to || signed_in_path(conn))
+    |> render("credentials.json", user)
+  end
 end
