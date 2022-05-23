abc1993c6adcbea0ef190263f5c122d572bc3cba
unenglishable <unenglishable@gmail.com>
2022-05-23 13:33:51 -1000

    feat(auth): use user_id as token subject

--- lib/epoch/guardian.ex
+++ lib/epoch/guardian.ex
@@ -1,24 +1,24 @@
 defmodule Epoch.Guardian do
   use Guardian, otp_app: :my_app
 
-  def subject_for_token(%{id: id}, _claims) do
+  def subject_for_token(%{user_id: user_id}, _claims) do
     # You can use any value for the subject of your token but
     # it should be useful in retrieving the resource later, see
     # how it being used on `resource_from_claims/1` function.
     # A unique `id` is a good subject, a non-unique email address
     # is a poor subject.
-    sub = to_string(id)
+    sub = to_string(user_id)
     {:ok, sub}
   end
   def subject_for_token(_, _) do
     {:error, :reason_for_error}
   end
 
-  def resource_from_claims(%{"sub" => id}) do
+  def resource_from_claims(%{"sub" => user_id}) do
     # Here we'll look up our resource from the claims, the subject can be
     # found in the `"sub"` key. In above `subject_for_token/2` we returned
     # the resource id so here we'll rely on that to look it up.
-    resource = Epoch.User.by_id(id)
+    resource = Epoch.User.by_id(user_id)
     {:ok,  resource}
   end
   def resource_from_claims(_claims) do
