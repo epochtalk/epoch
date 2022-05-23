c1b2ede94756d12dbc2761df5df2c29a94fb49a3
unenglishable <unenglishable@gmail.com>
2022-05-23 13:08:20 -1000

    feat(auth): add joken token_config
    maybe this will fix the empty_signer error

--- lib/epoch/jwt.ex
+++ lib/epoch/jwt.ex
@@ -1,3 +1,9 @@
 defmodule Epoch.JWT do
   use Joken.Config, default_signer: :pem_rs256
+
+  @impl Joken.Config
+  def token_config do
+    default_claims()
+    |> add_claim("role", fn -> "USER" end, &(&1 in ["ADMIN", "USER"]))
+  end
 end
