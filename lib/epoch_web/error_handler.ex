ae7f4de1e9f376d114f10cc168f44f921d470f19
unenglishable <unenglishable@gmail.com>
2022-05-27 14:06:39 -1000

    docs(auth): add todo for using error view in error handler

--- lib/epoch_web/error_handler.ex
+++ lib/epoch_web/error_handler.ex
@@ -1,11 +1,11 @@
 defmodule EpochWeb.AuthErrorHandler do
   import Plug.Conn
-  alias EpochWeb.ErrorView
 
   @behaviour Guardian.Plug.ErrorHandler
 
   @impl Guardian.Plug.ErrorHandler
   def auth_error(conn, {type, reason}, opts) do
+    # TODO: try using error view here
     conn
     |> put_resp_content_type("text/plain")
     |> send_resp(401, inspect({type, reason}))
