6aff16c43ecaf9a69967ab297400bc00b1fb17ec
unenglishable <unenglishable@gmail.com>
2022-05-27 14:06:02 -1000

    refactor(auth): remove logging and comments from error handler

--- lib/epoch_web/error_handler.ex
+++ lib/epoch_web/error_handler.ex
@@ -6,13 +6,7 @@ defmodule EpochWeb.AuthErrorHandler do
 
   @impl Guardian.Plug.ErrorHandler
   def auth_error(conn, {type, reason}, opts) do
-    IO.puts("Auth error:")
-    IO.inspect(type)
-    IO.inspect(reason)
     conn
-    # |> put_view(ErrorView)
-    # |> render("401.json", %{message: inspect({type, reason})})
-    # |> render("401.json", %{message: inspect(changeset.errors)})
     |> put_resp_content_type("text/plain")
     |> send_resp(401, inspect({type, reason}))
   end
