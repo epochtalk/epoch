71ecc8613b7b96b697ddd6862dd3bec5cfdbc30f
unenglishable <unenglishable@gmail.com>
2022-06-07 10:46:44 -1000

    docs(auth): refactor comments, remove commented code

--- lib/epoch_web/views/error_view.ex
+++ lib/epoch_web/views/error_view.ex
@@ -7,14 +7,14 @@ defmodule EpochWeb.ErrorView do
   #   %{errors: %{detail: "Internal Server Error"}}
   # end
 
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.json" becomes
-  # "Not Found".
   def render("400.json", %{conn: conn}) do
     %{message: conn.assigns.reason.message}
   end
+
+  # By default, Phoenix returns the status message from
+  # the template name. For example, "404.json" becomes
+  # "Not Found".
   def template_not_found(template, _assigns) do
-    # %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
     %{message: Phoenix.Controller.status_message_from_template(template)}
   end
 end
