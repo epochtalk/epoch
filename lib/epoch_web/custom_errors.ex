70d92228e78f860c7c77be2b4b37840e81f9b8e9
unenglishable <unenglishable@gmail.com>
2022-06-23 12:06:57 -1000

    feat(auth): create NotLoggedIn error

--- lib/epoch_web/custom_errors.ex
+++ lib/epoch_web/custom_errors.ex
@@ -5,4 +5,10 @@ defmodule EpochWeb.CustomErrors do
     """
     defexception plug_status: 400, message: "Invalid credentials", conn: nil, router: nil
   end
+  defmodule NotLoggedIn do
+    @moduledoc """
+    Exception raised when user is not logged in
+    """
+    defexception plug_status: 400, message: "Not logged in", conn: nil, router: nil
+  end
 end
