2f6c72202ec95e45e7ae74b7f71f9d578910b63f
unenglishable <unenglishable@gmail.com>
2022-08-09 12:12:36 -1000

    feat(roles-permissions): implement process_roles_permissions.js
    processes roles_permissions.json file
    
    * output all permissions -> permissions.json
    
    * output priority restrictions -> priority_restrictions.json

---
+++ process_roles_permissions.js
@@ -0,0 +1,47 @@
+let rolesPermissions = require('./roles_permissions.json')
+const fs = require('fs')
+
+function flattenObject(ob) {
+    var toReturn = {};
+
+    for (var i in ob) {
+        if (!ob.hasOwnProperty(i)) continue;
+
+        if ((typeof ob[i]) == 'object' && ob[i] !== null) {
+            var flatObject = flattenObject(ob[i]);
+            for (var x in flatObject) {
+                if (!flatObject.hasOwnProperty(x)) continue;
+
+                toReturn[i + '.' + x] = flatObject[x];
+            }
+        } else {
+            toReturn[i] = ob[i];
+        }
+    }
+    return toReturn;
+}
+
+let allPermissions = {}
+let priorityRestrictions = {}
+// process permissions for each role
+Object.keys(rolesPermissions).forEach(function(role) {
+  console.log(role)
+  // get permissions out of role permissions for this role
+  let permissions = flattenObject(rolesPermissions[role])
+  // filter priorityRestrictions for each role
+  Object.keys(permissions).forEach(key => {
+    if (key.indexOf('priorityRestrictions') == 0) {
+      console.log(role + '.priorityRestrictions', rolesPermissions[role].priorityRestrictions)
+      priorityRestrictions[role] = rolesPermissions[role].priorityRestrictions
+      // // priorityRestrictions[role] = priorityRestrictions[role]
+      delete permissions[key]
+    }
+  })
+  allPermissions = {...allPermissions, ...permissions}
+})
+let allPermissionsString = JSON.stringify(Object.keys(allPermissions).sort(), null, 2)
+let priorityRestrictionsString = JSON.stringify(priorityRestrictions, null, 2)
+console.log(allPermissionsString)
+console.log(priorityRestrictionsString)
+fs.writeFileSync('permissions.json', allPermissionsString)
+fs.writeFileSync('priority_restrictions.json', priorityRestrictionsString)
