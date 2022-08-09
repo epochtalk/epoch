4fea02f13dfadb0b0fb9c208ce03b81307725b29
unenglishable <unenglishable@gmail.com>
2022-08-09 13:58:17 -1000

    docs(permissions): remove commented code

--- priv/repo/seed_permissions.exs
+++ priv/repo/seed_permissions.exs
@@ -5,7 +5,6 @@ alias Epoch.Repo
 alias Ecto.Multi
 
 create_permission_changeset = fn(data) ->
-# Permission.changeset(%Permission{}, )
   %{path: data}
 end
 
@@ -20,5 +19,3 @@ Multi.new()
 # |> Multi.delete_all(:delete_all, Permissions.all)
 |> Multi.insert_all(:insert_all, Permission, permissions_changesets)
 |> Repo.transaction()
-
-# |> IO.inspect
