e429802aa33aa22bb96c5f983d7cb0bd4fea5e69
unenglishable <unenglishable@gmail.com>
2022-08-09 14:20:52 -1000

    feat(permissions): implement priority_restrictions schema

---
+++ lib/epoch/priority_restriction.ex
@@ -0,0 +1,24 @@
+defmodule Epoch.PriorityRestriction do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Epoch.Repo
+  alias Epoch.PriorityRestriction
+
+  schema "priority_restrictions" do
+    field :role_lookup, :string
+    field :restrictions, :list
+  end
+
+  def changeset(priority_restrictions, attrs \\ %{}) do
+    priority_restrictions
+    |> cast(attrs, [:role_lookup, :restrictions])
+    |> validate_required([:role_lookup, :restrictions])
+  end
+  def by_lookup(role_lookup)
+      when is_binary(role_lookup) do
+    Repo.get_by(PriorityRestriction, role_lookup: role_lookup)
+  end
+  def all() do
+    Repo.all(PriorityRestriction)
+  end
+end
