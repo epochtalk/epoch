20bc9d126bad3ebc9dd0d7b454d9b84ab174dc85
unenglishable <unenglishable@gmail.com>
2022-08-11 12:07:05 -1000

    feat(roles-permissions): implement seed_priority_restrictions

---
+++ priv/repo/seed_priority_restrictions.exs
@@ -0,0 +1,11 @@
+json_file = "#{__DIR__}/../../priority_restrictions.json"
+
+alias Epoch.Role
+
+## only insert priority restrictions if they have not been set already
+
+roles_changeset = json_file
+|> File.read!()
+|> Jason.decode! # []
+# for each priority_restriction, set in db
+|> Enum.each(Role.set_priority_restrictions_by_lookup)
