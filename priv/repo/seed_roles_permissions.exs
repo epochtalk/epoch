1d5cb1814acca3f4c729e343a0feaf224078d335
unenglishable <unenglishable@gmail.com>
2022-08-18 16:30:05 -1000

    feat(roles-permissions): generate changeset maps from roles permissions json

--- priv/repo/seed_roles_permissions.exs
+++ priv/repo/seed_roles_permissions.exs
@@ -6,14 +6,14 @@ alias Epoch.Role
 # alias Ecto.Multi
 
 create_role_permission_changeset = fn({role_lookup, permissions}) ->
-  # permissions
-  # |> Enum.each()
-  # permissions
-  # |> Iteraptor.to_flatmap
-  # |> IO.inspect
   role = Role.by_lookup(role_lookup)
          |> IO.inspect
 
+  permissions
+  |> Iteraptor.to_flatmap
+  |> Enum.map(fn {permission_path, _value} ->
+    %{ role_id: role.id, permission_path: permission_path, value: true, modified: false}
+  end)
 end
 
 RolePermission.maybe_init
