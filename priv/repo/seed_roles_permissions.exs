243babeb89caa713d573c5d3fd2447afb6a46f5a
unenglishable <unenglishable@gmail.com>
2022-08-18 16:27:36 -1000

    feat(roles-permissions): use role.by_lookup in roles permissions seed

--- priv/repo/seed_roles_permissions.exs
+++ priv/repo/seed_roles_permissions.exs
@@ -1,17 +1,18 @@
 json_file = "#{__DIR__}/../../roles_permissions.json"
 
 alias Epoch.RolePermission
+alias Epoch.Role
 # alias Epoch.Repo
 # alias Ecto.Multi
 
-create_role_permission_changeset = fn({role, permissions}) ->
+create_role_permission_changeset = fn({role_lookup, permissions}) ->
   # permissions
   # |> Enum.each()
-  # role
-  # |> IO.inspect
   # permissions
   # |> Iteraptor.to_flatmap
   # |> IO.inspect
+  role = Role.by_lookup(role_lookup)
+         |> IO.inspect
 
 end
 
