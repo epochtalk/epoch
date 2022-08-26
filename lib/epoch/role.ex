c4131dc59f7156ccea49292a5acfd362ae5c08cd
Anthony Kinsey <anthonyjk@gmail.com>
2022-08-25 16:00:24 -1000

    refactor: code cleanup, and fix errors with changeset

--- lib/epoch/role.ex
+++ lib/epoch/role.ex
@@ -21,31 +21,25 @@ defmodule Epoch.Role do
 
   def changeset(role, attrs \\ %{}) do
     role
-    |> cast(attrs, [:name, :description, :lookup, :priority, :base_permissions, :custom_permissions])
-    |> validate_required([:name, :description, :lookup, :priority, :base_permissions, :custom_permissions])
+    |> cast(attrs, [:name, :description, :lookup, :priority, :permissions])
+    |> validate_required([:name, :description, :lookup, :priority, :permissions])
   end
+  def all(), do: Repo.all(Role)
+  def by_lookup(lookup), do: Repo.get_by(Role, lookup: lookup)
   def insert([]), do: {:error, "Role list is empty"}
-  def insert(%Role{} = role) do
-    Repo.insert(role)
-  end
-  def insert([%{}|_] = roles) do
-    Repo.insert_all(Role, roles)
-  end
-  def by_lookup(lookup) do
-    from(r in Role, where: r.lookup == ^lookup)
-    |> Repo.one
-  end
+  def insert(%Role{} = role), do: Repo.insert(role)
+  def insert([%{}|_] = roles), do: Repo.insert_all(Role, roles)
+
   def set_priority_restrictions_by_lookup({lookup, priority_restrictions}) do
     # only insert priority restrictions if they have not been set already
     from(r in Role, where: r.lookup == ^lookup and is_nil(r.priority_restrictions))
     |> Repo.update_all(set: [priority_restrictions: priority_restrictions])
   end
+
   def set_permissions(id, permissions) do
-    from(r in Role, where: r.id == ^id)
-    |> Repo.update_all(set: [permissions: permissions])
-  end
-  def all() do
     Role
-    |> Repo.all
+    |> Repo.get(id)
+    |> change(%{ permissions: permissions })
+    |> Repo.update
   end
 end
