1ef7290f18a2dc4a3766cab8e1611ac86334891d
Anthony Kinsey <anthonyjk@gmail.com>
2022-08-26 14:51:55 -1000

    feat: functioning permissions init, refactored seed role permissions script

--- priv/repo/seed_roles_permissions.exs
+++ priv/repo/seed_roles_permissions.exs
@@ -2,35 +2,36 @@ json_file = "#{__DIR__}/../../roles_permissions.json"
 
 alias Epoch.RolePermission
 alias Epoch.Role
-# alias Epoch.Repo
-# alias Ecto.Multi
+alias Epoch.Repo
 
+# helper function to create role permissions changeset beore upsert
 create_role_permission_changeset = fn({role_lookup, permissions}) ->
   role = Role.by_lookup(role_lookup)
-         |> IO.inspect
-
   permissions
   |> Iteraptor.to_flatmap
-  |> Enum.map(fn {permission_path, _value} ->
-    %{ role_id: role.id, permission_path: permission_path, value: true, modified: false}
+  |> Enum.map(fn {permission_path, value} ->
+    %{ role_id: role.id, permission_path: permission_path, value: value, modified: false}
   end)
 end
 
-## careful, we might need to use Multi here...
-# these queries need to execute in order
-RolePermission.maybe_init
-|> IO.inspect
+# maybe init RP, set RP values from file, update each role's permissions
+Repo.transaction(fn ->
+  # init if necessary
+  RolePermission.maybe_init!()
 
-roles_permissions_changesets = json_file
-|> File.read!()
-|> Jason.decode! # []
-|> Enum.map(create_role_permission_changeset) # changesets
-|> Enum.each(fn roles_permissions_changesets ->
-  RolePermission.upsert_value(roles_permissions_changesets)
+  # read actual permission values from file and update for each role
+  json_file
+  |> File.read!()
+  |> Jason.decode! # convert from json
+  |> Enum.map(create_role_permission_changeset)
+  |> Enum.each(&RolePermission.upsert_value(&1))
+
+  # generate permissions map for each role then update role
+  Role.all
+  |> Enum.map(fn role -> {role, RolePermission.permissions_map_by_role_id(role.id)} end)
+  |> Enum.each(fn {role, permissions} -> Role.set_permissions(role.id, permissions) end)
 end)
-
-#pseudocode
-Role.all
-|> Enum.map(fn role -> {role, RolePermission.permissions_json_by_role_id(role.id)} end)
-|> Enum.each(fn {role, permissions} -> Role.update_permissions_by_id(role.id, permissions) end)
-|> IO.inspect
+|> case do
+  {:ok, _} -> IO.puts("Successfully Seeded Role Permissions")
+  {:error, _} -> IO.puts("Error Seeding Role Permissions")
+end
